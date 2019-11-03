4ae6aac7f1b8217b90f5133b0acff3fd51c823f0
rahulramaprasad7 <54955817+rahulramaprasad7@users.noreply.github.com>
2019-11-03 01:54:40 -0700

    POST implemented

--- i2c.c
+++ i2c.c
@@ -11,13 +11,19 @@ void i2c_master_init()
 	PORTE->PCR[1] |= PORT_PCR_MUX(6) | 0x03;   //SDA
 
 	I2C1->F |= I2C_F_MULT(0) | I2C_F_ICR(0x11);   //Setting a baud rate of 300kbps
-	I2C1->C1|= I2C_C1_IICEN_MASK; //Enabling I2C module
+	I2C1->C1|= I2C_C1_IICEN_MASK;// | I2C_C1_IICIE_MASK; //Enabling I2C module
 
 	I2C1->C1 |= I2C_C1_TX_MASK;  //Enabling transmit mode
 	START;
-	DATA(0x90);  //Transmit first byte
+	DATA(0x91);  //Transmit first byte
 	WAIT;
-
+	if((I2C1->S & I2C_S_RXAK_MASK) == 0)           //Check if slave received a byte
+	{
+		printf("Temperature sensor detected\n");
+		I2C1->S |= I2C_S_RXAK_MASK;
+	}
+	else
+		disconnect();
 	DATA(0x02);  //Send pointer register address of TLow
 	WAIT;
 
@@ -63,8 +69,6 @@ uint16_t read_temp()
 		temp_read = (MSB << 4) | ((LSB >> 4) & 0x0F);
 		printf("%x\n", temp_read);
 		printf("The temperature is : %d\n", temp_read/16);
-		printf("%x\n",MSB);
-		printf("%x\n",LSB);
 	}
 	else
 	{
@@ -75,4 +79,10 @@ uint16_t read_temp()
 		printf("The temperature is : -%d\n", (temp_read)/16);
 	}
 	return temp_read;
+}
+
+void disconnect()
+{
+	printf("Temperature sensor not found\n");
+	while(1);
 }
