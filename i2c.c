0cf486ea70ebfd211c70f60d51a7942f8fef8829
rahulramaprasad7 <54955817+rahulramaprasad7@users.noreply.github.com>
2019-11-03 01:58:53 -0600

    I2C with macros

--- i2c.c
+++ i2c.c
@@ -1,8 +1,10 @@
 #include "../CMSIS/MKL25Z4.h"
 #include <stdio.h>
+#include "i2c.h"
+
 void i2c_master_init()
 {
-	//uint8_t data;
+
 	SIM->SCGC4 |= SIM_SCGC4_I2C1_MASK;  //Enabling the clock for I2C module
 	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
 	PORTE->PCR[0] |= PORT_PCR_MUX(6) | 0x03;   //SCL
@@ -11,68 +13,66 @@ void i2c_master_init()
 	I2C1->F |= I2C_F_MULT(0) | I2C_F_ICR(0x11);   //Setting a baud rate of 300kbps
 	I2C1->C1|= I2C_C1_IICEN_MASK; //Enabling I2C module
 
-	I2C1->C1 |= I2C_C1_TX_MASK;  //Setting the master bit and enabling transmit mode
-	I2C1->C1 |= I2C_C1_MST_MASK;
-	//	I2C1->A1 |= I2C_A1_AD(0x48);   //Setting the slave address
-	I2C1->D = I2C_D_DATA(0x90);   //Transmit first byte
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit first byte
-	I2C1->S |= I2C_S_IICIF_MASK;
+	I2C1->C1 |= I2C_C1_TX_MASK;  //Enabling transmit mode
+	START;
+	DATA(0x90);  //Transmit first byte
+	WAIT;
 
-	I2C1->D = I2C_D_DATA(0x02);  //Send pointer register address of TLow
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit pointer address
-	I2C1->S |= I2C_S_IICIF_MASK;
+	DATA(0x02);  //Send pointer register address of TLow
+	WAIT;
 
-	I2C1->D = I2C_D_DATA(0x16);  //Send MSB of TLow
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit pointer address
-	I2C1->S |= I2C_S_IICIF_MASK;
+	DATA(0x16);  //Send MSB of TLow
+	WAIT;
 
-	I2C1->D = I2C_D_DATA(0x00);  //Send LSB of TLow
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit pointer address
-	I2C1->S |= I2C_S_IICIF_MASK;
-//	I2C1->C1 &= ~I2C_C1_TXAK_MASK;  //NACK
-//	I2C1->C1 &= ~I2C_C1_MST_MASK;  //STOP
+	DATA(0x00);  //Send LSB of TLow
+	WAIT;
+}
 
-	I2C1->C1 |= I2C_C1_RSTA_MASK;  //RESTART
-	I2C1->D = I2C_D_DATA(0x90);   //Transmit first byte
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit first byte
-	I2C1->S |= I2C_S_IICIF_MASK;
-	I2C1->D = I2C_D_DATA(0x02);  //Send pointer register address of TLow
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit pointer address
-	I2C1->S |= I2C_S_IICIF_MASK;
-	I2C1->C1 |= I2C_C1_RSTA_MASK;
-	I2C1->D = I2C_D_DATA(0x90 | 0x01);  //Read TLOW
-	while(!(I2C1->S & I2C_S_IICIF_MASK));
-	I2C1->S |= I2C_S_IICIF_MASK;
-	I2C1->C1 &= ~I2C_C1_TXAK_MASK;
+uint16_t read_temp()
+{
+	uint8_t MSB = 0, LSB = 0;
+	uint16_t temp_read = 0;
+
+	RESTART;
+	DATA(0x90);   //Transmit first byte
+	WAIT;
+	DATA(0x00);  //Send pointer register address of Temperature
+	WAIT;
+	RESTART;
+
+	DATA(0x91);  //Read Temperature
+	WAIT;
+
+	//read mode
 	I2C1->C1 &= ~I2C_C1_TX_MASK;
+	//NACK
+	I2C1->C1 &= ~I2C_C1_TXAK_MASK;
 
-
+	//dummy read
 	I2C1->D;
-	while(!(I2C1->S & I2C_S_IICIF_MASK));
-	I2C1->S |= I2C_S_IICIF_MASK;
+	WAIT;
 
-	printf("TLOW Register contents\n");
-	uint8_t MSB = I2C1->D;
-	while(!(I2C1->S & I2C_S_IICIF_MASK));
-	I2C1->S |= I2C_S_IICIF_MASK;
-	printf("%x\n", MSB);
+	MSB = I2C1->D;
+	WAIT;
 
-	uint8_t LSB = I2C1->D;
-//	LSB = I2C1->D;
-	while(!(I2C1->S & I2C_S_IICIF_MASK));
-	I2C1->S |= I2C_S_IICIF_MASK;
-	printf("%x\n", LSB);
-//	I2C1->C1 &= ~I2C_C1_TXAK_MASK;  //NACK
+	STOP;
+	LSB = I2C1->D;
 
-//	if((!I2C1->S & I2C_S_RXAK_MASK))
-//		printf("Slave found\n");
-	I2C1->C1 |= I2C_C1_RSTA_MASK;  //RESTART
-	I2C1->D = I2C_D_DATA(0x90);   //Transmit first byte
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit first byte
-	I2C1->S |= I2C_S_IICIF_MASK;
-	I2C1->D = I2C_D_DATA(0x00);  //Send pointer register address of TLow
-	while(!(I2C1->S & I2C_S_IICIF_MASK)); //Wait to transmit pointer address
-	I2C1->S |= I2C_S_IICIF_MASK;
-//	I2C1->C1 |= I2C_C1_RSTA_MASK;
-	I2C1->D = I2C_D_DATA(0x90 | 0x01);
+	if((MSB & 0x80) == 0)
+	{
+		temp_read = (MSB << 4) | ((LSB >> 4) & 0x0F);
+		printf("%x\n", temp_read);
+		printf("The temperature is : %d\n", temp_read/16);
+		printf("%x\n",MSB);
+		printf("%x\n",LSB);
+	}
+	else
+	{
+		uint16_t twos_complement = (MSB << 4) | ((LSB >> 4) & 0x0F);
+		printf("%x\n", twos_complement);
+		temp_read = ((~twos_complement) & 0x0FF)  +1;
+		printf("%x\n", temp_read);
+		printf("The temperature is : -%d\n", (temp_read)/16);
+	}
+	return temp_read;
 }
